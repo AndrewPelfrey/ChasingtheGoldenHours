5ec5f851db20f88520a445839967e12db211a4e5
Garrett <garrettbritton34@gmail.com>
2024-04-03 20:03:23 -0400

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,6 @@
-<<<<<<< Updated upstream
-// GRABBING ELEMENTS FROM THE DOM
-let buttonEl = document.querySelector("#submit");
-let searchHistoryEl = document.querySelector("#search-history");
-let currentCity = document.querySelector("#current-location-input")
-let city = document.querySelector("#desired-location-input");
-
-
-// ADDING GOOGLE MAPS API
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-          ({key: "AIzaSyCV-KxEPg8JUzGUCcmCjBZv3hS5woEOVoM", v: "weekly"});
-
-// VARIABLE DECLARATION
-=======
->>>>>>> Stashed changes
 let map;
 let mapPreset;
 
-<<<<<<< Updated upstream
-async function initMap() {
-// CLEVELAND COORDINATES
-  const position = { lat: 41.4993, lng: -81.6944 };
-
-// IMPORTING MAP LIBRAIES
-  const { Map } = await google.maps.importLibrary("maps");
-
-// MAP CENTERING ON CLEVELAND, DECLARED BY mapID 
-  map = new Map(document.getElementById("map"), {
-=======
 function initMap() {
   // Cleveland coordinates
   mapPreset = { lat: 41.4993, lng: -81.6944 };
@@ -34,7 +8,6 @@ function initMap() {
   const { Map } = google.maps.importLibrary("maps");
 
   map = new google.maps.Map(document.getElementById("map"), {
->>>>>>> Stashed changes
     zoom: 11,
     center: mapPreset,
     mapId: "Cleveland",
@@ -42,9 +15,6 @@ function initMap() {
   });
 }
 
-<<<<<<< Updated upstream
-// JS FOR BULMA MODAL
-=======
 var directionsService = new google.maps.DirectionsService();
 
 var directionsDisplay = new google.maps.DirectionsRenderer(); 
@@ -83,7 +53,6 @@ initMap();
 
 
 // JS for Bulma Modal
->>>>>>> Stashed changes
 const openModalButton = document.getElementById('openModal');
 const closeModalButton = document.getElementById('closeModal');
 const modal = document.getElementById('myModal');
@@ -235,11 +204,7 @@ const themeSwitcher = document.getElementById("theme-switcher");
         }
     });
 
-<<<<<<< Updated upstream
-// ADDING CURRENT TIME STAMP
-=======
 // adding current time stamp
->>>>>>> Stashed changes
 const currentTime = dayjs().format(`h:mm A`);
 $(`#timeStamp`).text(`Time: ` + currentTime);
 
